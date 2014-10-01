684754ccfca713e1491953af55a2f3e433846bb5
Qiu Juntao <juntao.qiu@gmail.com>
2014-10-01 18:05:28 +1000

    make our application modulized

--- app.rb
+++ app.rb
@@ -8,29 +8,39 @@ require 'sqlite3'
 
 require './model/plants'
 
-env = ENV["ENV"] ? ENV["ENV"] : 'production'
-dbconfig = YAML.load(ERB.new(File.read(File.join("config","database.yml"))).result)
-ActiveRecord::Base.establish_connection(dbconfig[env])
-
-use Rack::PostBodyContentTypeParser
-
-get '/plants' do
-    plants = Plant.all || []
-    plants.to_json
-end
-
-get '/plants/:id' do
-    plant = Plant.find(params[:id])
-    plant.to_json 
-end
-
-post '/plants' do
-    plant = Plant.create(:name => params[:name],
-                        :description => params[:description],
-                        :created_at => Time.now,
-                        :updated_at => Time.now)
+class PlantApplication < Sinatra::Base
+    dbconfig = YAML.load(ERB.new(File.read(File.join("config","database.yml"))).result)
     
-    if plant.save
-        [201, "/plants/#{plant['id']}"]
+    configure :development do
+        ActiveRecord::Base.establish_connection(dbconfig['development'])
     end
+
+    configure :production do
+        ActiveRecord::Base.establish_connection(dbconfig['production'])
+    end
+
+    use Rack::PostBodyContentTypeParser
+
+    get '/plants' do
+        plants = Plant.all || []
+        plants.to_json
+    end
+
+    get '/plants/:id' do
+        plant = Plant.find(params[:id])
+        plant.to_json 
+    end
+
+    post '/plants' do
+        plant = Plant.create(:name => params[:name],
+                            :description => params[:description],
+                            :created_at => Time.now,
+                            :updated_at => Time.now)
+        
+        if plant.save
+            [201, "/plants/#{plant['id']}"]
+        end
+    end    
 end
+
+
